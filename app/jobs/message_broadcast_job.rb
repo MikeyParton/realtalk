dc6d903c501b06b252f4eeafc45a4169a8686db5
Mike Parton <mikeparton@live.com.au>
2016-10-15 19:04:49 -0300

    add subscription for new chats

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,14 +1,21 @@
 class MessageBroadcastJob < ApplicationJob  
   queue_as :default
 
-  def perform(message) 
+  def perform(message)
+      
+      if message.chat.messages[-2]
+          read_status = message.chat.messages[-2].read
+      else
+          read_status = false
+      end
+      
       ActionCable.server.broadcast "chat_#{message.chat_id}_channel",
                                   text:  message.text,
                                   user_id: message.user_id,
                                   chat_id: message.chat_id,
                                   date: message.created_at.strftime("%b %d"),
                                   time: message.created_at.strftime("%l:%M%p"),
-                                  last_read: message.chat.messages[-2].read
+                                  last_read: read_status 
   end 
 
   private 
